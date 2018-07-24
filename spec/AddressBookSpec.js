87cc3d9d0c6361f1c59772f783cf2480d4a6496c
iVailionyte <indre.vailionyte@gail.com>
2018-07-24 21:39:50 +0200

    Add another spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -3,7 +3,17 @@ describe('Address Book', function() {
     var addressBook = new AddressBook(),
       thisContact = new Contact();
 
-      addressBook.addContact(thisContact);
-      expect(addressBook.getContact(0)).toBe(thisContact);
+    addressBook.addContact(thisContact);
+    expect(addressBook.getContact(0)).toBe(thisContact);
 	});
+
+	it('should be able to delete a contact', function() {
+		var addressBook = new AddressBook(),
+		    thisContact = new Contact();
+    
+    addressBook.addContact(thisContact);
+    addressBook.deleteContact(0);
+
+    expect(addressBook.getContact(0)).not.toBeDefined();
+	})
 });
